84b04cb783cbee20ed8006cee933964731817a07
sandeep.i.32 <sandeep.i.learnweel@gmail.com>
2020-12-10 20:04:45 +0530

    bug: sloving bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <AppTany/>
+    {/* <NeoG/> */}
   </React.StrictMode>,
   rootElement
 );
